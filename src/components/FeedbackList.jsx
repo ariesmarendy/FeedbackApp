894bc900dc0e76889ddee61531a34f8762d2b9ec
Aries Marendy <ariesmarendy@pop-os.localdomain>
2022-07-13 09:05:19 +0700

    Added crud function to used fetch API

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,18 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import Feedbackitem from "./Feedbackitem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (feedback.length === 0) {
+  if (!isLoading && feedback.length === 0) {
     return <p>No feedback yet.</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner/>
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
